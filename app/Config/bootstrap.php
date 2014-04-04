1da1b7432fdf5c199876929df74b422978f92405
Fyre <fyr.work@gmail.com>
2014-04-04 02:26:20 +0200

    Убрал вывод через fdebug на не-тестовой среде

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -30,5 +30,7 @@ define('EMAIL_ADMIN_CC', 'fyr.work@gmail.com');
 define('PATH_FILES_UPLOAD', $_SERVER['DOCUMENT_ROOT'].'/files/');
 
 function fdebug($data, $logFile = 'tmp.log', $lAppend = true) {
-	file_put_contents($logFile, mb_convert_encoding(print_r($data, true), 'cp1251', 'utf8'), ($lAppend) ? FILE_APPEND : null);
+	if (TEST_ENV) {
+		file_put_contents($logFile, mb_convert_encoding(print_r($data, true), 'cp1251', 'utf8'), ($lAppend) ? FILE_APPEND : null);
+	}
 }
